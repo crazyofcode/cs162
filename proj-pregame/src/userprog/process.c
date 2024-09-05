56f9fcc843dd76c5b095dbab5a86209fca5aef38
crazyofcode <1710338943@qq.com>
2024-09-05 11:57:05 +0000

    finish proj0

--- proj-pregame/src/userprog/process.c
+++ proj-pregame/src/userprog/process.c
@@ -125,6 +125,7 @@ static void start_process(void* file_name_) {
      arguments on the stack in the form of a `struct intr_frame',
      we just point the stack pointer (%esp) to our stack frame
      and jump to it. */
+  asm volatile("movl $1, 4(%0)" : : "r" (if_.esp) : "memory");
   asm volatile("movl %0, %%esp; jmp intr_exit" : : "g"(&if_) : "memory");
   NOT_REACHED();
 }
@@ -473,7 +474,7 @@ static bool setup_stack(void** esp) {
   if (kpage != NULL) {
     success = install_page(((uint8_t*)PHYS_BASE) - PGSIZE, kpage, true);
     if (success)
-      *esp = PHYS_BASE;
+      *esp = PHYS_BASE - 20;
     else
       palloc_free_page(kpage);
   }
